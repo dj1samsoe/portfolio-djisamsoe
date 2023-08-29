9e6f5f00de3adb4ac614a4ad946ddc699a782d0e
dj1samsoe <dhanyh86@gmail.com>
2023-08-29 13:50:42 +0700

    edit responsive contact card

--- src/components/ContactCard.tsx
+++ src/components/ContactCard.tsx
@@ -11,7 +11,7 @@ interface ContactCardProps {
 export const ContactCard = (props: ContactCardProps) => {
   const { image, name, description, link } = props;
   return (
-    <motion.div className="flex items-center space-x-5 w-full md:w-max min-w-[400px] p-4 rounded-xl bg-zinc-800">
+    <motion.div className="flex items-center space-x-5 w-full md:w-max min-w-[300px] p-4 rounded-xl bg-zinc-800">
       <div className="w-20 h-20">{image}</div>
       <div className="flex flex-col space-y-2 items-start text-gray-400 text-sm">
         <div>
